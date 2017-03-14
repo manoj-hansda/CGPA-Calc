9be56f0f02f3712e7c99404acdbf1a86d021cf70
manoj-hansda <me.manojhansda@gmail.com>
2017-03-15 00:24:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,15 @@ $(document).ready(function(){
         $('input:checked').parent('.btn-block label').addClass("btn-success");
     });    
 });
+
+$.getJSON( "https://api.ethexindia.com/ticker/", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
